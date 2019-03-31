30a9055ba49c9d872c3ad9fb92207ab6116fbf17
Magnus Ottenklinger <evnu@posteo.de>
2019-03-31 16:10:49 +0200

    Prepare publishing hex package

--- mix.exs
+++ mix.exs
@@ -7,6 +7,9 @@ defmodule Docception.MixProject do
       version: "0.3.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/evnu/docception",
       deps: deps(),
       dialyzer: [
         ignore_warnings: "dialyzer.ignore-warnings",
@@ -24,6 +27,17 @@ defmodule Docception.MixProject do
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.5", only: [:dev], runtime: false},
       {:temp, "~> 0.4"}
+    ]
+  end
+
+  defp description do
+    "Running Elixir doctests on Markdown files"
+  end
+
+  defp package do
+    [
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/evnu/docception"}
     ]
   end
 end
