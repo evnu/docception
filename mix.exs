448f941af9a4a640a2dd16da48267f12f6591144
Magnus Ottenklinger <evnu@posteo.de>
2019-03-31 16:14:34 +0200

    deps: Add ex_doc

--- mix.exs
+++ mix.exs
@@ -24,6 +24,7 @@ defmodule Docception.MixProject do
 
   defp deps do
     [
+      {:ex_doc, "~> 0.19.3", only: :dev, runtime: false},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.5", only: [:dev], runtime: false},
       {:temp, "~> 0.4"}
