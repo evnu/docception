evnu/docception

commit c4ed2bb6736ab3ce576111a1ad332e122d38c27d

Stop ex_unit on shutdown

lib/mix/tasks/docception.ex
@@ -24,10 +24,9 @@ defmodule Mix.Tasks.Docception do
     Docception.run(files, verbose?)
   rescue
     e in Docception.Error ->
-      # Try to give the group_leader some time to write the message
-      sleep = 1000
-      IO.puts("Docception: Giving the group_leader #{sleep} ms to write")
-      Process.sleep(sleep)
       Mix.raise("Docception: #{e.message}")
+  after
+    # Stop ExUnit manually in order to try to let it write the error messages.
+    Application.stop(:ex_unit)
   end
 end

commit 35f022d53345f023053d11781e7103f33bebbab3

Stop ExUnit on shutdown only if there was an error
Error introduced with c4ed2bb, see #2.

lib/mix/tasks/docception.ex
@@ -22,11 +22,15 @@ defmodule Mix.Tasks.Docception do
     verbose? = true
 
     Docception.run(files, verbose?)
+
+    System.at_exit(fn
+      0 -> :ok
+      _ ->
+        # Stop ExUnit manually in order to try to let it write the error messages.
+        Application.stop(:ex_unit)
+    end)
   rescue
     e in Docception.Error ->
       Mix.raise("Docception: #{e.message}")
-  after
-    # Stop ExUnit manually in order to try to let it write the error messages.
-    Application.stop(:ex_unit)
   end
 end
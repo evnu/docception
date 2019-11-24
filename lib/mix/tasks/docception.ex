35f022d53345f023053d11781e7103f33bebbab3
Magnus Ottenklinger <evnu@posteo.de>
2019-11-24 21:47:25 +0100

    Stop ExUnit on shutdown only if there was an error
    Error introduced with c4ed2bb, see #2.

--- lib/mix/tasks/docception.ex
+++ lib/mix/tasks/docception.ex
@@ -22,11 +22,15 @@ defmodule Mix.Tasks.Docception do
     verbose? = true
 
     Docception.run(files, verbose?)
+
+    System.at_exit(fn
+      0 -> :ok
+      _ ->
+        # Stop ExUnit manually in order to try to let it write the error messages.
+        Application.stop(:ex_unit)
+    end)
   rescue
     e in Docception.Error ->
       Mix.raise("Docception: #{e.message}")
-  after
-    # Stop ExUnit manually in order to try to let it write the error messages.
-    Application.stop(:ex_unit)
   end
 end
